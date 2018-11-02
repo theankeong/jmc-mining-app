1091217935146b20d29ae10408325583e9c40c38
theankeong <theankeong@gmail.com>
2018-11-02 14:18:56 +0800

    Updated KV

---
+++ KV.js
@@ -0,0 +1,33 @@
+async function f_getsecrets() {
+    const KeyVault = require('azure-keyvault');
+    const { AuthenticationContext } = require('adal-node')
+    var clientId = 'd90c76b1-4238-46ad-b470-871eb53391fe';
+    var clientSecret = 'ZY3LZWFMamdyA5Au7DI8SS58IYtCpd1QWmx434FvCg4=';
+    var result2 = '';
+
+    var secretAuthenticator = function (challenge, callback) {
+
+        var context = new AuthenticationContext(challenge.authorization);
+        return context.acquireTokenWithClientCredentials(
+            challenge.resource,
+            clientId,
+            clientSecret,
+            function (err, tokenResponse) {
+                if (err) throw err;
+
+                var authorizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
+                return callback(null, authorizationValue);
+            });
+    };
+
+    const secretUrl = "https://myc4tskv.vault.azure.net/secrets/myc4tscosmosdb/cc9492c5c5b54222be2008cc86c39f14";
+    var credentials = new KeyVault.KeyVaultCredentials(secretAuthenticator);
+    var client = new KeyVault.KeyVaultClient(credentials);
+
+    var result = await client.getSecret("https://myc4tskv.vault.azure.net", "myc4tscosmosdb", "cc9492c5c5b54222be2008cc86c39f14");
+        
+
+    return result;
+    //console.log(result.value);
+}
+module.exports.f_getsecrets = f_getsecrets;
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var express = require('express'),
     assert = require('assert'),
     ObjectId = require('mongodb').ObjectID,
      
-    url = 'mongodb://myc4ts:6E4ks7zaCBxIy59C39rHuAFlO9SddfJ6CuSuWPlPSEMnuIFmpM3Fh80XHcfQfWdCVCEY2cw7POXpjod3nHM0PA==@myc4ts.documents.azure.com:10255/simplemean?ssl=true&replicaSet=globaldb';   
+   // url = 'mongodb://myc4ts:6E4ks7zaCBxIy59C39rHuAFlO9SddfJ6CuSuWPlPSEMnuIFmpM3Fh80XHcfQfWdCVCEY2cw7POXpjod3nHM0PA==@myc4ts.documents.azure.com:10255/simplemean?ssl=true&replicaSet=globaldb';   
+   url = '';   
    
 
     app.use(express.static(__dirname + "/public"));
@@ -29,7 +30,13 @@ function errorHandler(err, req, res, next) {
 
 async function main()
 {
-    //url ="AAA"
+    
+     //KV
+     var kvsvc = require('./KV');
+     var kvsecrets = await kvsvc.f_getsecrets();
+     console.log(kvsecrets);
+    url =kvsecrets.value
+     //KV
     MongoClient.connect(process.env.MONGODB_URI || url,function(err, db){
         assert.equal(null, err);
        console.log('Successfully connected to MongoDB.');
