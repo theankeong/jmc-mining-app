2cb2550ba019580855098f9ff1d622b956498973
Lee Hsiang <leehsiang@outlook.com>
2018-11-06 02:16:52 +0800

    Update KV.js
    fixed environment variables and made it const.

--- KV.js
+++ KV.js
@@ -2,11 +2,11 @@ async function f_getsecrets() {
     
     const KeyVault = require('azure-keyvault');
     const { AuthenticationContext } = require('adal-node')
-    var clientId = process.env.CLIENT_ID;
-    var clientSecret = process.env.CLIENT_SECRET;
-    var secretUrl = process.env.SECRET_URL ;
-    var vaultName = process.env.VAULT_NAME ;
-    var vaultKey = process.env.VAULT_KEY ;   
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const secretUrl = process.env.SECRET_URL ;
+    const vaultName = process.env.VAULT_NAME ;
+    const vaultKey = process.env.VAULT_KEY ;   
            
     var secretAuthenticator = function (challenge, callback) {
 
