f9b4f3e2833a0ac2469916187e638ebbcade233b
Lee Hsiang <leehsiang@outlook.com>
2018-11-06 00:45:06 +0800

    Update KV.js
    Revert to env['']

--- KV.js
+++ KV.js
@@ -2,11 +2,11 @@ async function f_getsecrets() {
     
     const KeyVault = require('azure-keyvault');
     const { AuthenticationContext } = require('adal-node')
-    var clientId = process.env['container:APPSETTING_clientId'] ;
-    var clientSecret = process.env['container:APPSETTING_clientSecret'] ;
-    var secretUrl = process.env['container:APPSETTING_secretUrl'] ;
-    var vaultName = process.env['container:APPSETTING_vaultName'] ;
-    var vaultKey = process.env['container:APPSETTING_vaultKey'] ;   
+    var clientId = process.env['clientId'] ;
+    var clientSecret = process.env['clientSecret'] ;
+    var secretUrl = process.env['secretUrl'] ;
+    var vaultName = process.env['vaultName'] ;
+    var vaultKey = process.env['vaultKey'] ;   
        
     
     var secretAuthenticator = function (challenge, callback) {
