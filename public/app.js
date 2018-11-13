67de8393eb9890c6ff259033e1d766ed407db3e1
theankeong <theankeong@gmail.com>
2018-11-13 16:04:10 +0800

    Update public/app.js

--- public/app.js
+++ public/app.js
@@ -24,7 +24,7 @@ function AppCtrl($scope, $http) {
     vm.getAllRecords = function () {
         $http.get('/records').then(function (response) {
             vm.records = response.data;
-            vm.maxSize = 10;
+            vm.maxSize = 20;
             vm.bigTotalItems = vm.records.length;            
             vm.bigCurrentPage = 1;
         }, function (response) {
