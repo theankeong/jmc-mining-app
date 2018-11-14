09c19358c0c2319a77e74f41c28cbef08ee975cb
theankeong <theankeong@gmail.com>
2018-11-14 17:01:12 +0800

    aaaa

--- KV.js
+++ KV.js
@@ -6,15 +6,7 @@ async function f_connstring() {
     const vaultUrl = process.env.VAULT_URL ;
     const vaultName = process.env.VAULT_NAME ;
     const vaultKey = process.env.VAULT_KEY ;
-/*
-const KeyVault = require('azure-keyvault');
-const { AuthenticationContext } = require('adal-node')
-const clientId = "d90c76b1-4238-46ad-b470-871eb53391fe";
-const clientSecret = "ZY3LZWFMamdyA5Au7DI8SS58IYtCpd1QWmx434FvCg4=";
-const vaultUrl = "https://myc4tskv.vault.azure.net";
-const vaultName = "myc4tscosmosdb";
-const vaultKey = "cc9492c5c5b54222be2008cc86c39f14";
-*/
+
     var secretAuthenticator = function (challenge, callback) {
 
         var context = new AuthenticationContext(challenge.authorization);
@@ -50,15 +42,7 @@ async function f_redistring() {
     const vaultName = process.env.VAULT_REDISNAME ;
     const vaultKey = process.env.VAULT_REDISKEY ;
 
-/*
-const KeyVault = require('azure-keyvault');
-const { AuthenticationContext } = require('adal-node')
-const clientId = "d90c76b1-4238-46ad-b470-871eb53391fe";
-const clientSecret = "ZY3LZWFMamdyA5Au7DI8SS58IYtCpd1QWmx434FvCg4=";
-const vaultUrl = "https://myc4tskv.vault.azure.net";
-const vaultName = "myc4tsredis";
-const vaultKey = "b7ed7d4d24a34b8a8ee962463a11c438";
-*/
+
 
     var secretAuthenticator = function (challenge, callback) {
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-//const appInsights=require('applicationinsights');
-//appInsights.setup('00b84d21-5a94-4a3c-a11a-6b5ed2e01024').start();
+const appInsights=require('applicationinsights');
+appInsights.setup('00b84d21-5a94-4a3c-a11a-6b5ed2e01024').start();
 
 var express = require('express'),
     app = express(),
